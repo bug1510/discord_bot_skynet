fd6dfbe4e6fbcfa2a10977fb611d8d9d8a0f7ca3
Chris <mokkujin@gmx.de>
2021-08-10 17:13:20 +0200

    some little tweaks

--- cogs/simple.py
+++ cogs/simple.py
@@ -1,10 +1,8 @@
-import time
-import discord
-import time,os
-import requests
-import discord
+import time,os,requests,discord,logging,pafy
+from discord.ext.commands import bot
 from discord.ext import commands
-import logging
+from discord.utils import get
+
 logger = logging.getLogger(__name__)
 source = os.path.dirname(os.path.abspath(__file__))
 class Just4Fun(commands.Cog):
@@ -20,6 +18,7 @@ class Just4Fun(commands.Cog):
         """Langeweile? Mr Meeseeks erzählt dir gerne ein paar Witze | Mögliche Werte für Language sind cs, fr, en, de, es, pt."""
 
         member = ctx.message.author
+        await ctx.message.delete()
         logger.info(str(member) + ' called joke')
         logger.info('displayname : ' + str(member.display_name))
         joke_req = requests.get('https://v2.jokeapi.dev/joke/Any?lang=' + language)
@@ -44,10 +43,12 @@ class Just4Fun(commands.Cog):
             time.sleep(0.5)
             await ctx.send(answer)
         
+        
     @commands.command()
     async def cat(self,context):
 
         """Du willst was süßes sehen? Dann lass dir ein Kätzchen zeigen."""
+        
         cat_pic = requests.get('https://cataas.com/cat?json=true')
         cat_pic_json = cat_pic.json()
         pic_url = cat_pic_json['url']
@@ -60,14 +61,16 @@ class Just4Fun(commands.Cog):
             url= 'https://cataas.com/' + str(pic_url)
         )
         await context.send(embed=mbed)
+        await context.message.delete()
 
     @commands.command()
 
     async def cat2(self,ctx):
 
         """Du willst was süßes sehen? Dann lass dir ein Kätzchen zeigen."""
+
         # download file
-        CacheFile = source + '/../cat.jpg'
+        CacheFile = source + '/../tmp/cat.jpg'
         URLFile = requests.get('http://thecatapi.com/api/images/get?format=src&type=jpg')
 
         with open(CacheFile,'wb') as file:
@@ -80,12 +83,14 @@ class Just4Fun(commands.Cog):
         embed.set_image(url="attachment://cat.png")
 
         await ctx.send(file=file,embed=embed)
+        await ctx.message.delete()
 
     @commands.command()
 
     async def dog(self,ctx):
 
         """ süße Hunde gibts hier """
+        
         dog_pic = requests.get('https://random.dog/woof.json')
         dog_pic_json = dog_pic.json()
         dog_pic_url = dog_pic_json['url']
@@ -98,6 +103,8 @@ class Just4Fun(commands.Cog):
             url = dog_pic_url
         )
         await ctx.send(embed=embed)
+        await ctx.message.delete()
+
 
 def setup(bot):
     bot.add_cog(Just4Fun(bot))
