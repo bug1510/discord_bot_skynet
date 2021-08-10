a4e614cff0b219c5e9f0cb3118a1bd7f16e2ae69
bug1510 <scorptech1510@gmail.com>
2021-08-10 15:19:43 +0200

    show me a god damn cat

--- cogs/simple.py
+++ cogs/simple.py
@@ -43,6 +43,23 @@ class Just4Fun(commands.Cog):
             await ctx.send(question)
             time.sleep(0.5)
             await ctx.send(answer)
+        
+    @commands.command()
+    async def catpic(self,context):
+
+        """Du willst was süßes sehen? Dann lass dir ein Kätzchen zeigen."""
+        cat_pic = requests.get('https://cataas.com/cat?json=true')
+        cat_pic_json = cat_pic.json()
+        pic_url = cat_pic_json['url']
+        mbed = discord.Embed(
+            title='Cutie',
+            description='meow',
+            color=discord.Color.purple()
+            )
+        mbed.set_image(
+            url= 'https://cataas.com/' + str(pic_url)
+        )
+        await context.send(embed=mbed)
 
     @commands.command()
 
