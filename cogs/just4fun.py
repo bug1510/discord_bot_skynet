da4506ba51125176f96d86cad7f977e8bd8ef5e6
Chris <mokkujin@gmx.de>
2021-08-13 16:34:31 +0200

    rename simple.py to just4fun.py - create sound.py

--- cogs/just4fun.py
+++ cogs/just4fun.py
@@ -117,36 +117,6 @@ class Just4Fun(commands.Cog):
         await ctx.message.delete()
 
     @commands.command()
-    async def clown(self, ctx):
-
-        """ top secret """
-
-        CacheFile = source + '/../tmp/sound.mp3'
-        URLFile = requests.get('http://api.pleaseclown.me/')
-        with open(CacheFile, 'wb') as file:
-            file.write(URLFile.content)
-
-        #############################################
-        # need ffmpeg to play sound !               #
-        # on windows ffmpeg must be in the path var #
-        # ----------------------------------------- #
-        # pip install mutagen                       #
-        # pip install ffmpeg                        #
-        # apt install ffmpeg                        #
-        #############################################
-
-        voicechannel = discord.utils.get(ctx.guild.channels, name='quatschen')
-        vc = await voicechannel.connect()
-        vc.play(discord.FFmpegPCMAudio(CacheFile),
-                after=lambda e: print('done', e))
-        # Get Time
-        mf = MP3(CacheFile)
-        TrackLength = int(mf.info.length) + 3
-        time.sleep(TrackLength)
-        await vc.disconnect()
-        await ctx.message.delete()
-
-    @commands.command()
     async def xkcd(self, ctx, pic='random'):
 
         """ show random pic from xkcd (Nerd Comics) 
---
+++ cogs/sounds.py
@@ -0,0 +1,51 @@
+import time
+import os
+import requests
+import discord
+import logging
+import random
+from mutagen.mp3 import MP3
+from discord.ext.commands import bot
+from discord.ext import commands
+from discord.utils import get
+
+logger = logging.getLogger(__name__)
+source = os.path.dirname(os.path.abspath(__file__))
+
+
+class Sounds(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def clown(self, ctx):
+
+        """ top secret """
+
+        CacheFile = source + '/../tmp/sound.mp3'
+        URLFile = requests.get('http://api.pleaseclown.me/')
+        with open(CacheFile, 'wb') as file:
+            file.write(URLFile.content)
+
+        #############################################
+        # need ffmpeg to play sound !               #
+        # on windows ffmpeg must be in the path var #
+        # ----------------------------------------- #
+        # pip install mutagen                       #
+        # pip install ffmpeg                        #
+        # apt install ffmpeg                        #
+        #############################################
+
+        voicechannel = discord.utils.get(ctx.guild.channels, name='quatschen')
+        vc = await voicechannel.connect()
+        vc.play(discord.FFmpegPCMAudio(CacheFile),
+                after=lambda e: print('done', e))
+        # Get Time
+        mf = MP3(CacheFile)
+        TrackLength = int(mf.info.length) + 3
+        time.sleep(TrackLength)
+        await vc.disconnect()
+        await ctx.message.delete()
+
+def setup(bot):
+    bot.add_cog(Sounds(bot))
