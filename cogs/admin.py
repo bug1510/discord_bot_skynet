b4937427f7bc092ff52773d2858e29dc82e542e3
bug1510 <scorptech1510@gmail.com>
2021-08-26 16:37:35 +0200

    New Error Handling and embeds for gcc and gcd

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,15 +1,22 @@
-import discord
+import discord, os, json
 from discord.ext import commands
 from discord.utils import get
 import logging
 logger = logging.getLogger(__name__)
+
+source = os.path.dirname(os.path.abspath(__file__))
+config_file = source + '/../config.json'
+
+conf = open(config_file)
+maintenance = json.load(conf)
+conf.close()
 
 class AdminCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name='gcc')
-    @commands.has_role('El-Special')
+    @commands.has_role(maintenance['maintananceRole'])
 
     async def game_channel_create(self, context, gamename):
 
@@ -18,16 +25,49 @@ class AdminCommands(commands.Cog):
         guild = context.message.guild
         member = context.message.author
         logger.info(str(member) + ' tried to create category ' + str(gamename))
-        gamerole = await guild.create_role(name=gamename)
         junior_admin = get(context.guild.roles, name='Junior Admin')
         admin = get(context.guild.roles, name='Admin')
         senior_admin = get(context.guild.roles, name='Senior Admin')
-        
+        user = member.display_name
+        embed = discord.Embed(
+            title = 'GameChannelCreate',
+            description = f'Das Erstellen wurde von {user} ausgelöst.',
+            color = discord.Color.dark_gold()
+            )
+
+        try:
+            gamerole = await guild.create_role(name=gamename)
+            logger.info(str(member) + ' created category bound role ' + str(gamename) + ' successfully')
+            embed = embed.add_field(
+                name= '!Success creating Category connected Role!',
+                value= 'Die Rolle ' + str(gamename) + ' wurde erstellt.',
+                inline=False
+            )
+        except:
+            logger.info(str(member) + ' tried to create category bound role ' + str(gamename) + ' but it failed')
+            embed = embed.add_field(
+                name= '!Failure creating Category connected Role!',
+                value= 'Die Rolle ' + str(gamename) + ' wurde nicht erstellt,\nbitte prüfe deine Konfiguration.',
+                inline=False
+            )
+
         try:
             place = await guild.create_category_channel(name='<##>' + str(gamename))
+            logger.info('Category ' + str(place) + ' was created by ' + str(member))
+            embed = embed.add_field(
+                name= '!Success creating Category!',
+                value= 'Der Game Channel ' + str(place) + ' wurde erstellt.',
+                inline=False
+            )
+        except:
+            logger.warning('Category ' + str(place) + ' wasnt created')
+            embed = embed.add_field(
+                name= '!Failure creating Category!',
+                value= 'Der Game Channel ' + str(place) + ' konnte nicht erstellt.',
+                inline=False
+            )
 
-            logger.info('Category ' + str(gamename) + ' was created by ' + str(member))
-
+        try:
             await place.set_permissions(
                 guild.default_role,
 
@@ -173,31 +213,68 @@ class AdminCommands(commands.Cog):
                 request_to_speak=True
                 )
 
-            logger.info('Permission for the category ' + str(gamename) + ' was set')
-            await context.send('Der Game Channel ' + str(gamename) + ' wurde erstellt und die Berechtigungen wurden gesetzt.')
+            logger.info('Permissions for the category ' + str(place) + ' were set')
+            embed = embed.add_field(
+                name= '!Success setting Permissions!',
+                value= 'Die Berechtigungen für den Game Channel ' + str(place) + ' gesetzt.',
+                inline=False
+            )
         except:
-            logger.warning('Failed to create category ' + str(gamename) + ' and/or setting Permission')
-            await context.send('Beim erstellen vom Game Channel ' + str(gamename) + ' etwas schief gelaufen bitte überprüfe deine Angaben und probiere es dann nochmal.')
+            logger.warning('Failed to set Permissions for the Categorie ' + str(place))
+            embed = embed.add_field(
+                name= '!Failure setting Permissions!',
+                value= 'Beim setzen der Berechtigung für den Game Channel\n ' + str(place) + ' ist etwas schief gelaufen bitte überprüfe die Config.',
+                inline=False
+            )
 
         try:
-            await guild.create_text_channel(name=str(gamename) + '-talk', category=place)
-        
-            vc = ['Fraktion-I', 'Fraktion-II', 'Fraktion-III', 'Fraktion-IV', 'Group-Talk']
-        
-            for c in vc:
-                if c.find('Group'):
-                    await guild.create_voice_channel(name=c, category=place, user_limit=8)
-                else:
-                    await guild.create_voice_channel(name=c, category=place)
-            logger.info('The Voice and Text Channels for ' + str(place) + ' were created')
-            await context.send('Alle Channels für ' + str(place) + ' wurden erstellt')
+            tc = maintenance['standardTextChannels']
+
+            await guild.create_text_channel(name=str(gamename), category=place)
+
+            for c in tc:
+                await guild.create_text_channel(name=c, category=place)
+
+            logger.info('The Text Channel for ' + str(place) + ' was created')
+            embed = embed.add_field(
+                name= '!Success creating Text Channels!',
+                value= 'Der Text Channel für ' + str(place) + ' wurde erstellt',
+                inline=False
+            )
         except:
-            logger.warning('While creating the Channels for ' + str(place) + ' went something wrong please check input or config.')
-            await context.send('Beim erstellen der Channels für ' + str(place) + ' ist etwas schief gegangen, überprüfe bitte deine Angaben oder die Konfiguration.')
+            logger.warning('While creating the Test Channel for ' + str(place) + ' went something wrong please check your server config.')
+            embed = embed.add_field(
+                name= '!Failure creating Text Channels!',
+                value= 'Beim erstellen des Text Channel für ' + str(place) + ' ist etwas schief gegangen,\nüberprüfe bitte deine Server Konfiguration.',
+                inline=False
+            )
+
+        try:
+            vc = maintenance['standardVoiceChannels']
+
+            for c in vc:
+                userlimit = maintenance['userLimit']
+                await guild.create_voice_channel(name=c, category=place, user_limit=userlimit)
+            
+            logger.info('The Voice Channels for ' + str(place) + ' were created')
+            embed = embed.add_field(
+                name= '!Success creating Voice Channels!',
+                value= 'Die Voice Channels für ' + str(place) + ' wurden erstellt.',
+                inline=False
+            )
+        except:
+            logger.warning('While creating the Voice Channels for ' + str(place) + ' went something wrong please check your server config.')
+            embed = embed.add_field(
+                name= '!Failure creating Voice Channels',
+                value= 'Beim erstellen der Voice Channels für ' + str(place) + ' ist etwas schief gegangen,\nüberprüfe bitte deine Server Konfiguration.',
+                inline=False
+            )
+            
+        await context.send(embed=embed)
         await context.message.delete()
         
     @commands.command('gcd')
-    @commands.has_role('El-Special')
+    @commands.has_role(maintenance['maintananceRole'])
 
     async def game_channel_delete(self, context, gamename):
 
@@ -206,40 +283,101 @@ class AdminCommands(commands.Cog):
         member = context.message.author
         gamerole = get(guild.roles, name=gamename)
         place = get(guild.categories, name= '<##>' + str(gamename))
+        user = member.display_name
+        embed = discord.Embed(
+            title = 'GameChannelDelete',
+            description = f'Das Löschen wurde von {user} ausgelöst.',
+            color = discord.Color.dark_gold()
+            )
         
         try:
             for tc in place.text_channels:
                 await tc.delete(reason=None)
-
+            
+            logger.info(str(member) + ' deleted all Text Channels from the Category ' + str(gamename) + ' successfully')
+            embed = embed.add_field(
+                name= '!Success deleting Text Channels!',
+                value= 'Die Text Channels aus ' + str(place) + ' wurden gelöscht',
+                inline=False
+            )
+        except:
+            logger.warning(str(member) + ' tried to delete all Text Channels from the Category' + str(gamename) + ' but it failed')
+            embed = embed.add_field(
+                name= '!Failure deleting Text Channels',
+                value= 'Beim löschen der Text Channels für ' + str(place) + ' ist etwas schief gegangen,\nüberprüfe bitte deine Server Konfiguration.',
+                inline=False
+            )
+        
+        try:
             for vc in place.voice_channels:
                 await vc.delete(reason=None)
-
-            await place.delete(reason=None)
-            await gamerole.delete(reason=None)
             
-            logger.warning(str(member) + ' deleted ' + str(gamename) + ' successfully')
-            await context.send('Der Abschnitt ' + str(place) + ' wurde erfogreich von deinem Sever gelöscht.')
-
+            logger.info(str(member) + ' deleted all Voice Channels from the Category ' + str(gamename) + ' successfully')
+            embed = embed.add_field(
+                name= '!Success deleting Voice Channels!',
+                value= 'Die Voice Channels für ' + str(place) + ' wurden gelöscht.',
+                inline=False
+            )
         except:
-            logger.warning(str(member) + ' tried to delete ' + str(gamename) + ' but it failed')
-            await context.send('Da ist wohl etwas schief gelaufen überprüfe ob du alles richtig geschrieben hast!')
-        await context.message.delete()
+            logger.warning(str(member) + ' tried to delete all Voice Channels from the Category ' + str(gamename) + ' but it failed')
+            embed = embed.add_field(
+                name= '!Failure deleting Voice Channels',
+                value= 'Beim löschen der Voice Channels für ' + str(place) + ' ist etwas schief gegangen,\nüberprüfe bitte deine Server Konfiguration.',
+                inline=False
+            )
 
-    @commands.command()
-    @commands.has_role('El-Special')
-    async def clear(self, context, number):
-        channel = context.message.channel
-        admin = context.message.author.display_name
-        embed = discord.Embed(
-            title='!ACHTUNG!',
-            description=f'{number} alte Nachrichten wurden aus diesem Chat gelöscht,\nvon deinem Admin: {admin}',
-            color=discord.Color.dark_red())
+        try:
+            await place.delete(reason=None)
 
-        await channel.purge(limit=int(number), oldest_first=True, bulk=False)
+            logger.info(str(member) + ' deleted the Category ' + str(gamename) + ' successfully')
+            embed = embed.add_field(
+                name= '!Success deleting Category!',
+                value= 'Die Kategorie ' + str(place) + ' wurde gelöscht.',
+                inline=False
+            )
+        except:
+            logger.warning(str(member) + ' tried to delete the Category ' + str(gamename) + ' but it failed')
+            embed = embed.add_field(
+                name= '!Failure deleting Category!',
+                value= 'Beim löschen der Categrory ' + str(place) + ' ist etwas schief gegangen,\nüberprüfe bitte deine Server Konfiguration.',
+                inline=False
+            )
+
+        try:
+            await gamerole.delete(reason=None)
+
+            logger.info(str(member) + ' deleted the role to the Category ' + str(gamename) + ' successfully')
+            embed = embed.add_field(
+                name= '!Success deleting Category connected Role!',
+                value= 'Die zu ' + str(place) + ' gehörige Rolle wurde gelöscht.',
+                inline=False
+            )
+        except:
+            logger.warning(str(member) + ' tried to delete the role to the Category ' + str(gamename) + ' but it failed')
+            embed = embed.add_field(
+                name= '!Failure deleting Category connected Role!',
+                value= 'Beim löschen der zu ' + str(place) + ' gehörigen Rolle ist etwas schief gelaufen.',
+                inline=False
+            )
+
         await context.send(embed=embed)
         await context.message.delete()
 
-
+    @commands.command()
+    @commands.has_role(maintenance['maintananceRole'])
+    async def clear(self, context, number):
+        channel = context.message.channel
+        member = context.message.author
+        user = context.message.author.display_name
+        embed = discord.Embed(
+            title='!ACHTUNG!',
+            description=f'{number} alte Nachrichten wurden aus diesem Chat gelöscht,\nvon deinem Admin: {user}',
+            color=discord.Color.dark_red())
+            
+        await context.message.delete()
+        logger.info(f'{number} alte Nachrichten wurden aus diesem Chat gelöscht, vom Admin: {member}')
+        await channel.purge(limit=int(number), oldest_first=True, bulk=False)
+        await context.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(AdminCommands(bot))
