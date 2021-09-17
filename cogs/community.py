2947f8017a8bcdde60aaa2f2635fc29f1a89e865
bug1510 <scorptech1510@gmail.com>
2021-09-17 20:48:38 +0200

    .

--- cogs/community.py
+++ cogs/community.py
@@ -3,19 +3,9 @@ from discord.ext.commands.core import command
 import sqlite3, json, os
 from leveling.lvl import exp_gain
 
-class ShirosSakeBar(commands.Cog):
+class CommunityCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command(name='leveling_on')
-    async def init_leveling_db(self, ctx):
-        guild_id = ctx.message.author.guild.id
-        con = sqlite3.connect(f'{guild_id}.db')
-        cur = con.cursor()
-        cur.execute("""Create Table IF NOT EXISTS leveling
-        (dc_user_nick text, dc_user_id integer, tw_user_nick text, tw_user_id integer, level integer NOT NULL DEFAULT 1, exp integer NOT NULL DEFAULT 0)""")
-        con.commit()
-        con.close()
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -25,4 +15,4 @@ class ShirosSakeBar(commands.Cog):
             await exp_gain(message)
 
 def setup(bot):
-    bot.add_cog(ShirosSakeBar(bot))
+    bot.add_cog(CommunityCommands(bot))
