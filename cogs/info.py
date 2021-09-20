812b7013034ab50a30ba2ef939cd81ae0b020cf3
bug1510 <scorptech1510@gmail.com>
2021-09-20 16:09:14 +0200

    added communityinfo and removed restriction

--- cogs/info.py
+++ cogs/info.py
@@ -1,18 +1,22 @@
-import discord,os
-from discord import client
+import discord,os, json
+from discord import colour
 from discord.ext import commands
 from discord.utils import get
 import logging,platform,psutil
 logger = logging.getLogger(__name__)
 source = os.path.dirname(os.path.abspath(__file__))
 
+config_file = source + '/../config.json'
+
+conf = open(config_file)
+maintenance = json.load(conf)
+conf.close()
+
 class InfoCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name='serverinfo')
-    @commands.has_role('El-Special')
-
     async def get_serverinfo(self, context):
         """ Show Server Informations """
         embed = discord.Embed(title='Serverinformations',
@@ -41,7 +45,38 @@ class InfoCommands(commands.Cog):
         
         await context.message.delete()
 
+    @commands.command(name='communityinfo')
+    async def get_communityinfo(self, ctx):
+        """ Show all Infos about the Community """
+
+        guild_icon = ctx.message.guild.icon_url_as(static_format='png', size=128)
+        guild_name = ctx.message.guild.name
+        guild_owner = ctx.message.guild.owner
+        guild_roles = ctx.message.guild.roles
+        guild_members = ctx.message.guild.member_count
+        twitch_url = maintenance['twitchLink']
+
+        number_of_roles = 0
+
+        for r in guild_roles:
+            number_of_roles += 1
+
+        embed = discord.Embed(title=f'Info about {guild_name}',color=colour.Color.blurple())
+
+        embed.set_thumbnail(url=guild_icon)
+
+        embed.add_field(name='Owner', value=guild_owner, inline=True)
+        
+        embed.add_field(name='Number of Roles', value=number_of_roles, inline=True)
+
+        embed.add_field(name='Number of Members(including Bots)', value=guild_members, inline=True)
+
+        embed.add_field(name=f'Twitch Link of {guild_owner}', value=twitch_url, inline=True)
+
+        await ctx.send(embed=embed)
+
+        await ctx.message.delete()
+
+
 def setup(bot):
     bot.add_cog(InfoCommands(bot))
-
-
