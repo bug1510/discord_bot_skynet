6466c3ba119a8146f1526dff52f2300582f1cd30
bug1510 <scorptech1510@gmail.com>
2021-09-23 11:19:45 +0200

    refactored meeseeks in  MeeseeksCore Class

--- meeseeks_bot.py
+++ meeseeks_bot.py
@@ -1,112 +1,103 @@
-#!/usr/bin/env python3
-
-import json
-import os,sys,getopt
-import logging
-from datetime import datetime
-import discord
+import discord, os, json, sys, getopt, logging
 from discord.ext import commands
-from discord.utils import get
+from datetime import datetime
 
 today = datetime.now()
 today = today.strftime("%Y%m%d")
 source = os.path.dirname(os.path.abspath(__file__))
-Version = '0.6.5 - ALPHA'
-BotName = 'Meeseeks Bot | V' + Version
 logpath = source + '/logs/'
 config_file = source + '/config.json'
 
-conf = open(config_file)
+conf = open(config_file, "r")
 maintenance = json.load(conf)
 conf.close()
 
-def usage():
-    print("""
-        Meeseeks Bot - have fun to use
-        
-        usage:
+#check your config.json file for standard entries
+initial_extension = maintenance['modules']
 
-            -h or --help    show this help
-            -c or --config  define the json configfile      (default : ./token.json)
-            -l or --log     define the logpath              (default ./logs/)
+intents = discord.Intents.default()
+intents.voice_states = True
+intents.members = True
 
-        example:
+class MeeseeksCore(commands.Bot):
+    def __init__(self, **options):
+        super().__init__(**options)
 
-            use mbot.json as config and write logfile to /var/log/
+    async def init_logger(self):
+        # logging
+        ## check log folder 
+        if not os.path.exists(logpath):
+            os.makedirs(logpath)
+        logging.basicConfig(
+            filename= logpath + '/' + str(today) + '_discord_bot.log',
+            format='%(asctime)s - %(name)s - %(levelname)s | %(message)s',
+            level=logging.INFO
+            )
+        self.logger = logging.getLogger(__name__)
 
-            meeseeks_bot.py -c /home/dev/document/mbot.json -l /var/log/
+    async def init_modules(self):
+        #this loop collects all modules in cogs and put them in initial_extension
+        for CogFile in os.listdir(source + '/cogs/'):
+            if CogFile.endswith('.py'):
+                module = 'cogs.' + CogFile[:-3]
+                if module not in initial_extension:
+                    initial_extension.append(module)
+                    self.logger.info('append cog - ' + module)
 
-    """)
+        #this loop loads the extension in the bot
+        for extension in initial_extension:
+            try:
+                client.load_extension(extension)
+            except Exception as e:
+                print(e)
 
+    async def on_ready(self):
+        await self.init_logger()
+        await self.init_modules()
+        await client.change_presence(activity=discord.Activity(name='.help', type=2))
+        self.logger.info('bot started')
+        self.logger.info('config file : ' + config_file)
+        self.logger.info('log path : ' + logpath)
+        print('initiated')
 
-# parameters for bot start
-try:
-    opts, args = getopt.getopt(sys.argv[1:],'c:l:h',['config','log','help'])
-except:
-    usage()
-    sys.exit(2)
+    def usage():
+        print("""
+            Meeseeks Bot - have fun to use
+            
+            usage:
 
-for opt, arg in opts:
-    if opt in ('-h','--help'):
-        usage()
-        sys.exit(1)
-    elif opt in ('-c','--config'):
-        config_file = arg
-    elif opt in ('-l','--log'):
-        logpath = arg
-    else:
+                -h or --help    show this help
+                -c or --config  define the json configfile      (default : ./token.json)
+                -l or --log     define the logpath              (default ./logs/)
+
+            example:
+
+                use mbot.json as config and write logfile to /var/log/
+
+                meeseeks_bot.py -c /home/dev/document/mbot.json -l /var/log/
+
+        """)
+
+    # parameters for bot start
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],'c:l:h',['config','log','help'])
+    except:
         usage()
         sys.exit(2)
 
-# logging
+    for opt, arg in opts:
+        if opt in ('-h','--help'):
+            usage()
+            sys.exit(1)
+        elif opt in ('-c','--config'):
+            config_file = arg
+        elif opt in ('-l','--log'):
+            logpath = arg
+        else:
+            usage()
+            sys.exit(2)
 
-## check log folder 
-if not os.path.exists(logpath):
-    os.makedirs(logpath)
-logging.basicConfig(
-    filename= logpath + '/' + str(today) + '_discord_bot.log',
-    format='%(asctime)s - %(name)s - %(levelname)s | %(message)s',
-    level=logging.INFO
-    )
-logger = logging.getLogger(__name__)
-
-# Das ist die finale Struktur des Meeseeks_Bots
-
-initial_extension = ['cogs.bot-managing']
-
-# load all cogs in ./cogs/
-for CogFile in os.listdir(source + '/cogs/'):
-    if CogFile.endswith('.py'):
-        module = 'cogs.' + CogFile[:-3]
-        if module not in initial_extension:
-            initial_extension.append(module)
-            logger.info('append cog - ' + module)
-
-# change no category in !help
-help_command = commands.DefaultHelpCommand(no_category = 'Commands')
-
-client = commands.Bot(
-    command_prefix='!',
-    description='Meeseeks Bot | V' + Version,
-    help_command = help_command
-    )
+client = MeeseeksCore(command_prefix='.', description=maintenance['botName'] + maintenance['version'], help_command = commands.DefaultHelpCommand(no_category = 'Help'))
 
 if __name__ == '__main__':
-    for extension in initial_extension:
-        try:
-            client.load_extension(extension)
-            logger.info('cog ' + extension + ' loaded')
-        except:
-            logger.warning('cog ' + extension + ' could not be loaded')
-            pass
-
-@client.event
-async def on_ready():
-
-    await client.change_presence(activity=discord.Activity(name='!help', type=2))
-    print('Ich bin bereit')
-    logger.info('bot started')
-    logger.info('config file : ' + config_file)
-    logger.info('log path : ' + logpath)
-
-client.run(maintenance['token'])
+    client.run(maintenance['token'])
